cf78db1f07cf00f6205f9c0b7b568a1835a093f2
Akrem Boughazi <kikoua203@gmail.com>
2023-11-23 03:43:23 +0100

    removing login register btn and add functionality for logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,19 @@
-import { Avatar, Box, Button, ButtonGroup, Container, IconButton, Menu, MenuButton, MenuDivider, MenuGroup, MenuItem, MenuList, Spacer, Stack, Text, useColorMode, } from '@chakra-ui/react'
+import { Avatar, Box, IconButton, Menu, MenuButton, MenuDivider, MenuGroup, MenuItem, MenuList, Spacer, Stack, Text, useColorMode, useToast, } from '@chakra-ui/react'
 import { FaMoon, FaSun } from 'react-icons/fa'
 import { IoMdLogOut } from 'react-icons/io'
-import { Link } from 'react-router-dom'
-import cookie from 'react-cookies'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { API_URL } from '../constants'
+import { useStore } from '../utils/store'
 type Props = {}
 
 const Navbar = (props: Props) => {
     const { colorMode, toggleColorMode } = useColorMode()
-    const token: { token: string } = cookie.load('token')
+    const navigate = useNavigate()
+    const toast = useToast()
+    ///// TODO : figure out how to use the cookies instead of the localStorage
+    const setToken = useStore(store => store.setToken)
+    const token = useStore(store => store.token)
     return (
         <Box position='sticky' top={0} p='4' zIndex={10} backdropFilter='auto' backdropBlur='8px' shadow='sm'>
             <Stack alignItems='center' direction='row' >
@@ -22,27 +28,45 @@ const Navbar = (props: Props) => {
                 <Spacer />
                 <IconButton onClick={toggleColorMode} variant='ghost' aria-label='change theme' icon={colorMode === "light" ? <FaMoon fill='teal' /> : <FaSun fill='gold' />} />
                 {
-                    token ?
-                        <Menu>
-                            <MenuButton as={Avatar} name='Dan Abrahmov' src='https://bit.ly/dan-abramov' size='sm' cursor='pointer' />
-                            <MenuList>
-                                <MenuGroup title='Profile'>
-                                    <MenuItem>User@example.com</MenuItem>
-                                </MenuGroup>
-                                <MenuDivider />
-                                <MenuGroup title='Setting'>
-                                    <MenuItem><IoMdLogOut style={{ marginRight: 2 }} /> Logout</MenuItem>
-                                </MenuGroup>
-                            </MenuList>
-                        </Menu>
-                        :
-                        <ButtonGroup gap='2'>
-                            <Button colorScheme={'teal'} variant={'outline'} size={{ base: 'sm', md: 'md' }}>Log In</Button>
-                            <Button colorScheme={'teal'} variant={'solid'} size={{ base: 'sm', md: 'md' }}>Register</Button>
-                        </ButtonGroup>
+                    token &&
+                    <Menu>
+                        {/* 
+                        // TODO: handle the avatar image.
+                        */}
+                        <MenuButton as={Avatar} name='User Name' src='https://bit.ly/dan-abramov' size='sm' cursor='pointer' />
+                        <MenuList>
+                            <MenuGroup title='Profile'>
+                                <MenuItem>User@example.com</MenuItem>
+                            </MenuGroup>
+                            <MenuDivider />
+                            <MenuGroup title='Setting'>
+                                {/* 
+                                // TODO: create handle logout function.
+                                */}
+                                <MenuItem
+                                    onClick={async () => await axios.get(`${API_URL}auth/logout`).then(res => {
+                                        setToken(null)
+                                        localStorage.removeItem('token')
+                                        navigate('/')
+                                    }).catch((error) => {
+                                        toast({
+                                            title: `${error.response?.data?.message || 'something went wrong'}`,
+                                            position: 'top',
+                                            status: 'error',
+                                            isClosable: true,
+                                        })
+                                    })
+                                    }
+                                >
+                                    <IoMdLogOut style={{ marginRight: 2 }} />
+                                    Logout
+                                </MenuItem>
+                            </MenuGroup>
+                        </MenuList>
+                    </Menu>
                 }
             </Stack>
-        </Box>
+        </Box >
     )
 }
 
