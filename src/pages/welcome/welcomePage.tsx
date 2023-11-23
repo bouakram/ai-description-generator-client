1698d2ce85881f723c049a947eb92dccf8e96808
Akrem Boughazi <kikoua203@gmail.com>
2023-11-23 03:45:08 +0100

    removing the layout archt

--- src/pages/welcome/welcomePage.tsx
+++ src/pages/welcome/welcomePage.tsx
@@ -1,17 +1,11 @@
-import Navbar from '../../components/Navbar'
-import Footer from '../../components/Footer'
 import WelcomeMain from '../../components/welcome/WelcomeMain'
 import { Stack } from '@chakra-ui/react'
 
 function Welcome() {
     return (
-        <div>
-            <Navbar />
-            <Stack width='100%' height='calc(100vh - 7rem)' p='4' alignContent='center' justifyContent='center'>
-                <WelcomeMain />
-            </Stack>
-            <Footer />
-        </div>
+        <Stack width='100%' height='calc(100vh - 7rem)' p='4' alignContent='center' justifyContent='center'>
+            <WelcomeMain />
+        </Stack>
     )
 }
 
