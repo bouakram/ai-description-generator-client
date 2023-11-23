050c1b4d4b7f33255fead213bddfbbc077a315a4
Akrem Boughazi <kikoua203@gmail.com>
2023-11-23 03:44:19 +0100

    adding ink to the welcom btn

--- src/components/welcome/WelcomeMain.tsx
+++ src/components/welcome/WelcomeMain.tsx
@@ -1,6 +1,5 @@
 import { Button, Card, CardBody, CardFooter, CardHeader, Heading, Text } from '@chakra-ui/react'
-import React from 'react'
-
+import { Link } from 'react-router-dom'
 type Props = {}
 
 const WelcomeMain = (props: Props) => {
@@ -13,7 +12,7 @@ const WelcomeMain = (props: Props) => {
                 <Text align='center' fontSize={{ base: 'small', md: 'unset' }}>Explore ContentGen, where AI meets content creation. Unleash your creativity effortlessly with personalized and high-quality posts generated to suit your unique style. Perfect for bloggers, influencer, and marketers, ContentGen transforms your content creation process, saving you time and elevating your online presence.</Text>
             </CardBody>
             <CardFooter alignContent='center'>
-                <Button colorScheme='teal' variant='solid' borderStart='1px' borderTop='1px' borderEnd='4px' borderBottom='4px' borderColor='teal' size={{ base: 'sm', md: 'md' }}>Start Now!</Button>
+                <Link to={'/authentication'}><Button colorScheme='teal' variant='solid' borderStart='1px' borderTop='1px' borderEnd='4px' borderBottom='4px' borderColor='teal' size={{ base: 'sm', md: 'md' }}>Start Now!</Button></Link>
             </CardFooter>
         </Card>
     )
