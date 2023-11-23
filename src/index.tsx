efbc82c6d66fad83b7ccad992794ec1fb5d3add3
Akrem Boughazi <kikoua203@gmail.com>
2023-11-23 03:42:37 +0100

    remouving the routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+
+// * dependencies 
 import { ColorModeScript } from '@chakra-ui/react'
-import theme from './config/themeconfg'
 import { ChakraProvider } from '@chakra-ui/react'
+import theme from './config/themeconfg'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+// * pages
 import './index.css';
 import App from './App';
-import cookie from 'react-cookies'
-import Home from './pages/home/homePage';
-import Welcome from './pages/welcome/welcomePage';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const TOKEN = cookie.load('token')
-console.log(TOKEN)
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    // element: TOKEN ? <App /> : <Welcome />,
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-    ],
-  },
-]);
+const queryClient = new QueryClient()
 
 root.render(
   <React.StrictMode>
     <ChakraProvider>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
