f5fecd650e5153eaee4a2209882f9d94122b57f2
Akrem Boughazi <kikoua203@gmail.com>
2023-11-23 03:41:36 +0100

    adding routes and protecting them

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
-import { Outlet } from "react-router-dom";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import Welcome from './pages/welcome/welcomePage';
+import AuthPage from './pages/authentication/authPage';
 
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import Main from "./pages/main/mainPage";
+import Layout from "./components/Layout";
+import { useStore } from './utils/store';
+
+///// ! when redirect to the path = '/' the page is not rerender after registration or logging in.
 function App() {
+  const token = useStore(store => store.token)
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: !token ? <Welcome /> : <Navigate to='/home' />,
+        },
+        {
+          path: "/home",
+          element: token ? <Main /> : <Navigate to='/' />,
+        },
+        {
+          path: "/authentication",
+          element: !token ? <AuthPage /> : <Navigate to='/' />,
+        },
+      ],
+    },
+  ]);
   return (
-    <div>
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
