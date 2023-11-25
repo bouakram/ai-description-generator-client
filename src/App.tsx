66260fba1c086bb627ac569bd1976328206d80e9
Akrem Boughazi <kikoua203@gmail.com>
2023-11-25 04:43:14 +0100

    adding auth verification for user

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,25 @@ import {
 import Main from "./pages/main/mainPage";
 import Layout from "./components/Layout";
 import { useStore } from './utils/store';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { API_URL } from './constants';
 
 ///// ! when redirect to the path = '/' the page is not rerender after registration or logging in.
 function App() {
   const token = useStore(store => store.token)
+  const setToken = useStore(store => store.setToken)
+  useQuery({
+    queryKey: ['verifyUser'],
+    queryFn: async () => {
+      const response = await axios.get(`${API_URL}auth/verify-user`, { withCredentials: true })
+      if (response.data.token) {
+        setToken(response.data.token)
+        localStorage.setItem('token', response.data.token)
+      }
+    },
+    retry: false
+  })
 
   const router = createBrowserRouter([
     {
