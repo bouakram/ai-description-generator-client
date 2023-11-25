bf83716d9f91ebc33211eca2aff01e898a21d5d1
Akrem Boughazi <kikoua203@gmail.com>
2023-11-25 04:47:31 +0100

    fixing issue by adding new schema for login

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,21 +3,18 @@ import { z } from "zod"
 export type AuthType = 'register' | 'login'
 
 export const RegisterFormSchema = z.object({
-    username: z.string(),
+    username: z.string().optional(),
     email: z.string().email(),
     password: z.string().min(8, { message: 'Must be 8 or more characters long' }),
-    confirmPassword: z.string()
+    confirmPassword: z.string().optional()
 }).refine(data => data.password === data.confirmPassword, {
     message: 'confirmed password must match the password',
     path: ['confirmPassword']
 })
+
 export const LoginFormSchema = z.object({
     email: z.string().email(),
-    password: z.string().min(8, "Must be 8 or more characters long"),
+    password: z.string().min(8, { message: 'Must be 8 or more characters long' }),
 })
 
-export type RegisterFormType = z.infer<typeof RegisterFormSchema>;
-export type LoginFormType = z.infer<typeof LoginFormSchema>
-
-
-
+export type FormType = z.infer<typeof RegisterFormSchema>;
