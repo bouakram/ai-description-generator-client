520af093dc019dcad300b043c231f5c5e061b6bf
Akrem Boughazi <kikoua203@gmail.com>
2023-11-25 04:46:42 +0100

    adding authType and setAuthType

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,13 +1,18 @@
 import { create } from "zustand";
+import { AuthType } from "../types/types";
 
 type StoreType = {
     token: string | null,
+    authType: AuthType,
     setToken: (token: string | null) => void
+    setAuthType: (token: AuthType) => void
 }
 
-const tokenString: string | null = localStorage.getItem('token') ? localStorage.getItem('token') : null
+let tokenString: StoreType["token"] = localStorage.getItem('token') ? localStorage.getItem('token') : null
 
 export const useStore = create<StoreType>((set) => ({
     token: tokenString,
+    authType: "register",
     setToken: (token) => set(() => ({ token: token })),
+    setAuthType: (authType) => set(() => ({ authType: authType }))
 }))
