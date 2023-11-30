5833117a6ecfb015e44748bb05784e56314b0cc8
Akrem Boughazi <kikoua203@gmail.com>
2023-11-30 03:46:44 +0100

    adding the components

--- src/pages/create/createPage.tsx
+++ src/pages/create/createPage.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import { Box, Flex, Heading } from '@chakra-ui/react'
+import Create from '../../components/create/form/Create'
+import Results from '../../components/create/results/Results'
+
+// TODO: make all the components work dynamically
 
 type Props = {}
 
 const CreatePage = (props: Props) => {
     return (
-        <div>
-            create Page
-        </div>
+        <Box p='4' width='full' minH='calc(100vh - 6rem)'>
+            <Heading mb={6} size={{ base: 'lg', md: 'xl' }}>Generate new Content 🤖📝</Heading>
+            <Flex direction={{ base: 'column', lg: 'row' }} minH='inherit' gap='6'>
+                <Create />
+                <Results />
+            </Flex>
+        </Box>
     )
 }
 
