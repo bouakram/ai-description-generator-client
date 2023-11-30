8c4888aa3c96d7abe790e013e747961bc066b037
Akrem Boughazi <kikoua203@gmail.com>
2023-11-30 03:44:19 +0100

    adding new folder with files contain constant data

---
+++ src/constants/data.ts
@@ -0,0 +1,21 @@
+export const createFormData = {
+    platform: [
+        "Facebook",
+        "InstaGram",
+        "LinkedIn",
+        "Twitter"
+    ],
+    tone: [
+        "Formal",
+        "Casual",
+        "Professional",
+        "Humorous",
+        "Inquisitive",
+        "Encouraging",
+        "Empathetic",
+        "Assertive",
+        "Optimistic",
+        "Sincere"
+    ],
+    pages: [1, 2, 3, 4, 5]
+} as const
