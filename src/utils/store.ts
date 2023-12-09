f53fe674fcb5ffd33f97dcac7f46b7430a0c86e7
Akrem Boughazi <kikoua203@gmail.com>
2023-12-09 02:33:00 +0100

    adding new states to the store and state managmet function

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -4,8 +4,21 @@ import { AuthType } from "../types/types";
 type StoreType = {
     token: string | null,
     authType: AuthType,
-    setToken: (token: string | null) => void
-    setAuthType: (token: AuthType) => void
+    topic: string,
+    platform: string,
+    contentGenerated: string[],
+    generating: boolean,
+    completed: boolean,
+    issue: boolean,
+    setToken: (token: string | null) => void,
+    setAuthType: (token: AuthType) => void,
+    setTopic: (topic: string) => void,
+    setPlatform: (platform: string) => void,
+    setContentGenerated: (post: string[]) => void,
+    resetContentGenerated: () => void,
+    setGenerating: (generating: boolean) => void,
+    setCompleted: (completed: boolean) => void,
+    setIssue: (issue: boolean) => void,
 }
 
 let tokenString: StoreType["token"] = localStorage.getItem('token') ? localStorage.getItem('token') : null
@@ -13,6 +26,19 @@ let tokenString: StoreType["token"] = localStorage.getItem('token') ? localStora
 export const useStore = create<StoreType>((set) => ({
     token: tokenString,
     authType: "register",
+    topic: "",
+    platform: "",
+    contentGenerated: [],
+    generating: false,
+    completed: false,
+    issue: false,
     setToken: (token) => set(() => ({ token: token })),
-    setAuthType: (authType) => set(() => ({ authType: authType }))
+    setAuthType: (authType) => set(() => ({ authType: authType })),
+    setTopic: (topic) => set(() => ({ topic: topic })),
+    setPlatform: (platform) => set(() => ({ platform: platform })),
+    setContentGenerated: (post) => set(() => ({ contentGenerated: post })),
+    resetContentGenerated: () => set(() => ({ contentGenerated: [] })),
+    setGenerating: (generating) => set(() => ({ generating: generating })),
+    setCompleted: (completed) => set(() => ({ completed: completed })),
+    setIssue: (issue) => set(() => ({ issue: issue })),
 }))
