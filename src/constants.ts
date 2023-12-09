be3603fd44dc90d82ab8824fd5751416d31616a9
Akrem Boughazi <kikoua203@gmail.com>
2023-12-09 15:10:54 +0100

    updating api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 //! cant reach the .env variable ?
-export const API_URL: string = process.env.API_URL || 'http://localhost:5000/api/v1/'
+// export const API_URL: string = process.env.API_URL || 'http://localhost:5000/api/v1/'
+export const API_URL: string = process.env.API_URL as string
