e814c33be287345b4e72a34c754208b121bbc605
Akrem Boughazi <kikoua203@gmail.com>
2023-12-09 02:40:27 +0100

    adding action function for the button

--- src/components/SubmitBtn.tsx
+++ src/components/SubmitBtn.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@chakra-ui/react'
 import React from 'react'
 
-// TODO: see what can add and what can remove from this component
+///// TODO: see what can add and what can remove from this component
 
 type SubmitBtnType = {
     children: React.ReactNode,
@@ -13,9 +13,9 @@ type SubmitBtnType = {
     size?: 'xs' | 'sm' | 'md' | 'lg',
 }
 
-const SubmitBtn = ({ children, loading, textloading, variant = 'solid', type = 'submit', size = 'md' }: SubmitBtnType) => {
+const SubmitBtn = ({ children, funcAction, loading, textloading, variant = 'solid', type = 'submit', size = 'md' }: SubmitBtnType) => {
     return (
-        <Button type={type} size={{ base: 'sm', md: size }} mb='4' colorScheme='teal' variant={variant} isLoading={loading} loadingText={textloading} spinnerPlacement='end' border='1px' borderEnd='4px' borderBottom='4px' borderColor='teal'>
+        <Button onClick={funcAction} type={type} size={{ base: 'sm', md: size }} mb='4' colorScheme='teal' variant={variant} isLoading={loading} loadingText={textloading} spinnerPlacement='end' border='1px' borderEnd='4px' borderBottom='4px' borderColor='teal'>
             {children}
         </Button>
     )
