9d36f0019e960cbd760c2c543b98b25902341d8f
Akrem Boughazi <kikoua203@gmail.com>
2023-12-09 17:39:06 +0100

    updating api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 //! cant reach the .env variable ?
 // export const API_URL: string = `${process.env.API_URL}/api/v1/` as string || 'http://localhost:5000/api/v1/'
-export const API_URL: string = 'https://contentgenapi.netlify.app/api/v1/'
+export const API_URL: string = 'https://contentgenapi.onrender.com/api/v1/'
