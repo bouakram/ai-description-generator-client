0a74df47b4e71922e6d94621a78b7b6136177d8d
Akrem Boughazi <kikoua203@gmail.com>
2023-12-09 02:41:16 +0100

    updating the create form component

--- src/components/create/form/Form.tsx
+++ src/components/create/form/Form.tsx
@@ -1,163 +1,65 @@
-import { FormControl, FormHelperText, FormLabel, Input, Radio, RadioGroup, Select, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Stack, Switch, Text, Tooltip } from '@chakra-ui/react'
+import { FormControl, Stack, Switch, Text } from '@chakra-ui/react'
 import SubmitBtn from '../../SubmitBtn'
 import { useState } from 'react'
-import { useForm } from 'react-hook-form'
 import { createFormData } from '../../../constants/data'
-import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import { GenerateContentService } from '../../../services/generateContentService'
+import RadioComponent from '../../formComponents/RadioComponent'
+import InputComponent from '../../formComponents/GenerateInputComponent'
+import SelectComponents from '../../formComponents/SelectComponents'
+import SliderComponent from '../../formComponents/SliderComponent'
+import SwitchComponent from '../../formComponents/SwitchComponent'
 
-type Props = {}
+// TODO: make all inputs disabled when generating the content
 
-const Form = (props: Props) => {
+const Form = () => {
     const [isTone, setIsTone] = useState(false)
-    const [sliderValue, setSliderValue] = useState(50)
-    const [showTooltip, setShowTooltip] = useState(false)
-    const { register, handleSubmit, formState: { errors }, setValue, reset } = useForm()
+    const { register, handleSubmit, onsubmit, isPending, setValue, errors } = GenerateContentService()
+
     const handletonecheck = () => {
         setIsTone(!isTone)
     }
-    const getPlatform = (socialMediaPlatform: string) => {
-        switch (socialMediaPlatform.toLowerCase()) {
-            case 'facebook':
-                return <FaFacebook />
-            case 'instagram':
-                return <FaInstagram />
-            case 'linkedin':
-                return <FaLinkedin />
-            case 'twitter':
-                return <FaTwitter />
-            default:
-                break;
-        }
-    }
 
-    const onsubmit = (data: any) => {
-        console.log(data)
-    }
     return (
         <form onSubmit={handleSubmit(onsubmit)}>
-            <FormControl isRequired mb='4'>
-                <FormLabel>Social media</FormLabel>
-                <RadioGroup defaultValue={createFormData.platform[0]}>
-                    <Stack spacing={5} direction='row' wrap='wrap'>
-                        {
-                            createFormData.platform.map(socialMediaPlatform => (
-                                <Radio key={socialMediaPlatform} {...register('Social_Media_Platform')} colorScheme='teal' value={socialMediaPlatform}>
-                                    <Stack alignItems='center' spacing={2} direction='row'>
-                                        {getPlatform(socialMediaPlatform)}
-                                        <Text>{socialMediaPlatform}</Text>
-                                    </Stack>
-                                </Radio>
-                            ))
-                        }
-                    </Stack>
-                </RadioGroup>
+            <FormControl isRequired mb='4' isDisabled={isPending}>
+                <RadioComponent data={createFormData} attr='platform' register={register} errors={errors} errorsMessage={errors.Social_Media_Platform?.message} name='Social_Media_Platform' helper='select the social media you want to generate for.' />
             </FormControl>
-            <FormControl isRequired mb='4'>
-                <FormLabel>Role</FormLabel>
-                <Input {...register('Role', { minLength: 3, maxLength: 35 })} type='text' placeholder='enter the role...' focusBorderColor='teal.500' />
-                <FormHelperText>select a role for generator.</FormHelperText>
+            <FormControl isRequired mb='4' isDisabled={isPending}>
+                <InputComponent name='Role' register={register} type='text' label='Role' placeholder='e.g: Software engineering | CopyWrite expert ....' errors={errors} errorsMessage={errors.Role?.message} helper='enter the role that uou want the generator to act like.' />
             </FormControl>
-            <FormControl isRequired mb='4'>
-                <FormLabel>Topic</FormLabel>
-                <Input {...register('Topic', { minLength: 3, maxLength: 35 })} type='text' placeholder='enter the topic...' focusBorderColor='teal.500' />
-                <FormHelperText>describe the topic you want to generate.</FormHelperText>
+            <FormControl isRequired mb='4' isDisabled={isPending}>
+                <InputComponent name='Topic' register={register} type='text' label='Topic' placeholder='e.g: create strawberry cake | road to a good developer ....' errors={errors} errorsMessage={errors.Topic?.message} helper='describe the topic you want to generate.' />
             </FormControl>
-            <FormControl isRequired mb='4'>
-                <FormLabel>Tone</FormLabel>
+            <FormControl isRequired mb='4' isDisabled={isPending}>
+                <InputComponent name='Audience' register={register} type='text' label='Audience' placeholder='e.g: mothers over 45 years old | girls | boys | students ....' errors={errors} errorsMessage={errors.Audience?.message} helper='select the audience you want to target.' />
+            </FormControl>
+            <FormControl isRequired mb='4' isDisabled={isPending}>
                 {
                     !isTone ?
-                        <>
-                            <Select {...register('Tone')} focusBorderColor='teal.500'>
-                                {
-                                    createFormData.tone.map(tone => (
-                                        <option key={tone} value={tone}>{tone}</option>
-                                    ))
-                                }
-                            </Select>
-                            <FormHelperText>select the tone you want</FormHelperText>
-                        </>
+                        <SelectComponents register={register} name='Tone' data={createFormData} attr='tone' errors={errors} errorsMessage={errors.Tone?.message} helper='select the tone you want' />
                         :
-                        <>
-                            <Input {...register('Tone', { minLength: 3, maxLength: 35 })} type='text' placeholder='enter your specific tone' focusBorderColor='teal.500' />
-                            <FormHelperText>enter the tone that you need for your generated content</FormHelperText>
-                        </>
+                        <InputComponent name='Tone' register={register} type='text' label='Tone' placeholder='e.g: Angry | Funny | Formal ....' errors={errors} errorsMessage={errors.Tone?.message} helper='enter the tone that you need for your generated content' />
                 }
                 <Stack direction='row' alignItems='center' spacing='4' mt='2'>
                     <Text>I want to enter a specific tone</Text>
                     <Switch colorScheme='teal' size='sm' onChange={handletonecheck} isRequired={false} />
                 </Stack>
             </FormControl>
-            <FormControl isRequired mb='4'>
-                <FormLabel>Words</FormLabel>
-                <Slider
-                    {...register('Number_of_Words')}
-                    id='slider'
-                    defaultValue={50}
-                    min={50}
-                    max={1000}
-                    colorScheme='teal'
-                    onChange={(v) => {
-                        setValue('Number_of_Words', v)
-                        setSliderValue(v)
-                    }
-                    }
-                    onMouseEnter={() => setShowTooltip(true)}
-                    onMouseLeave={() => setShowTooltip(false)}
-                >
-                    <SliderTrack bg='teal.100'>
-                        <SliderFilledTrack bg='teal' />
-                    </SliderTrack>
-                    <Tooltip
-                        hasArrow
-                        bg='teal.500'
-                        color='white'
-                        placement='top'
-                        isOpen={showTooltip}
-                        label={`${sliderValue} word`}
-                    >
-                        <SliderThumb />
-                    </Tooltip>
-                </Slider>
-                <FormHelperText>select how much words you need</FormHelperText>
+            <FormControl isRequired mb='4' isDisabled={isPending}>
+                <SliderComponent register={register} name='Number_of_Words' setValue={setValue} errors={errors} errorsMessage={errors.Number_of_Words?.message} helper='select how much words you need' />
             </FormControl>
-            <FormControl mb='4'>
-                <FormLabel>Hashtag</FormLabel>
-                <Stack alignItems='center' direction='row' spacing='5'>
-                    <Text>Include Hashtags</Text>
-                    <Switch {...register('Hashtags')} colorScheme='teal' isRequired={false} />
-                </Stack>
-                <FormHelperText>activate if you need hashtags related to the topic.</FormHelperText>
+            <FormControl mb='4' isDisabled={isPending}>
+                <SwitchComponent register={register} name='Hashtags' helper='activate if you need hashtags related to the topic.' isRequired={false} />
             </FormControl>
-            <FormControl mb='4'>
-                <FormLabel>Emoji</FormLabel>
-                <Stack alignItems='center' direction='row' spacing='5'>
-                    <Text>Include Emoji</Text>
-                    <Switch {...register('Emoji')} colorScheme='teal' isRequired={false} />
-                </Stack>
-                <FormHelperText>activate if you want emoji with the text generated.</FormHelperText>
+            {/* <FormControl mb='4'>
+                <SwitchComponent register={register} name='Emoji' helper='activate if you want emoji with the text generated.' isRequired={false} />
+            </FormControl> */}
+            <FormControl isRequired mb='4' isDisabled={isPending}>
+                <RadioComponent register={register} name='Number_of_Posts_to_Generate' data={createFormData} attr='pages' errors={errors} errorsMessage={errors.Number_of_Posts_to_Generate?.message} helper='select the number of posts you want to generate' />
             </FormControl>
-            <FormControl isRequired mb='4'>
-                <FormLabel>Number of Posts</FormLabel>
-                <RadioGroup defaultValue={String(createFormData.pages[0])}>
-                    <Stack spacing={5} direction='row' wrap='wrap'>
-                        {
-                            createFormData.pages.map((value) => (
-                                <Radio
-                                    key={value}
-                                    value={String(value)}
-                                    colorScheme='teal'
-                                    {...register('Number_of_Posts_to_Generate')}
-                                >
-                                    {value}
-                                </Radio>
-                            ))
-                        }
-                    </Stack>
-                </RadioGroup>
-                <FormHelperText>select how much posts you need.</FormHelperText>
+            <FormControl>
+                <SubmitBtn loading={isPending} textloading='generating' >Generate</SubmitBtn>
             </FormControl>
-
-            <SubmitBtn loading={false} textloading='generating' >Generate</SubmitBtn>
         </form>
     )
 }
