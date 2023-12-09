a311b3e1670338965630db66f26ee991a452fda3
Akrem Boughazi <kikoua203@gmail.com>
2023-12-09 02:35:24 +0100

    making the word cloud dinamic

--- src/components/main/WordCloudCard.tsx
+++ src/components/main/WordCloudCard.tsx
@@ -1,24 +1,13 @@
-import { Card, CardHeader, Stack, Text } from '@chakra-ui/react';
+import { Card, CardBody, CardHeader, Stack, Text } from '@chakra-ui/react';
 import WordCloud from 'react-d3-cloud';
 import { FaSearchengin } from 'react-icons/fa';
+import TopicServices from '../../services/topicServices';
+import NoContentText from '../NoContentText';
 
 type Props = {}
 
-const data = [
-  { text: 'first impression', value: 800 },
-  { text: 'Hey', value: 1000 },
-  { text: 'lol', value: 2000 },
-  { text: 'first impression', value: 800 },
-  { text: 'lol', value: 20 },
-  { text: 'very cool', value: 100 },
-  { text: 'Hey', value: 10000 },
-  { text: 'duck', value: 10 },
-  { text: 'very cool', value: 1000 },
-  { text: 'duck', value: 1000 },
-  { text: 'duck', value: 10 },
-]
-
 const WordCloudCard = (props: Props) => {
+  const { data, isPending, isError, error } = TopicServices()
   return (
     <Card border='1px' borderEnd='4px' borderBottom='4px' borderColor='inherit' w='full' h='full'>
       <CardHeader>
@@ -27,21 +16,33 @@ const WordCloudCard = (props: Props) => {
           <FaSearchengin style={{ height: 35, width: 35 }} />
         </Stack>
       </CardHeader>
-      <WordCloud
-        data={data}
-        height={280}
-        font="Times"
-        // fontStyle="italic"
-        fontWeight="bold"
-        fontSize={(word) => Math.log2(word.value) * 5}
-        // spiral="rectangular"
-        rotate={(word) => word.value % 100}
-        padding={8}
-        random={Math.random}
-      // onWordClick={(event, d) => {
-      //   console.log(`onWordClick: ${d.text}`);
-      // }}
-      />
+      {
+        isPending ?
+          <CardBody>
+            <Text>waiting for data ...</Text>
+          </CardBody>
+          :
+          isError ?
+            <CardBody>
+              <Text color='red.400'>{error?.message}</Text>
+            </CardBody>
+            :
+            (data && data.length > 0) ? <WordCloud
+              data={data}
+              height={280}
+              font="Times"
+              rotate={0}
+              padding={10}
+              fontSize={(word) => Math.log2(word.value) * 5}
+            // onWordClick={(event, d) => {
+            //   console.log(`onWordClick: ${d.text}`);
+            // }}
+            />
+              :
+              <CardBody>
+                <NoContentText />
+              </CardBody>
+      }
     </Card>
   )
 }
