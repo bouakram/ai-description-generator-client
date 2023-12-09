ecafad1e681b60814efaefbc1c3d7df5efaffde7
Akrem Boughazi <kikoua203@gmail.com>
2023-12-09 02:36:03 +0100

    making the lastgen dinamic

--- src/components/main/LastGenCard.tsx
+++ src/components/main/LastGenCard.tsx
@@ -1,10 +1,15 @@
-import { Card, CardHeader, Stack, Text } from '@chakra-ui/react'
+import { Card, CardBody, CardHeader, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
-import { FaFacebook, FaInstagram, FaLinkedin, FaList } from 'react-icons/fa'
+import LastContentService from '../../services/lastContentService'
+import usePlatformSelector from '../../hooks/usePlatformSelector'
+import { FaList } from 'react-icons/fa'
+import NoContentText from '../NoContentText'
 
 type Props = {}
 
 const LastGenCard = (props: Props) => {
+    const { data, isPending, isError, error } = LastContentService()
+    const { getPlatform } = usePlatformSelector()
     return (
         <Card border='1px' borderEnd='4px' borderBottom='4px' borderColor='inherit' w='full' h='full'>
             <CardHeader>
@@ -14,36 +19,30 @@ const LastGenCard = (props: Props) => {
                 </Stack>
             </CardHeader>
             <Stack direction='column' spacing="2" p={6}>
-                <Card p='2' _hover={{ transform: 'translateY(-2px)', transition: 'all .2s ease-in-out', shadow: 'md', cursor: 'pointer' }}>
-                    <Stack direction='row' align='center' spacing='4'>
-                        <FaFacebook />
-                        <Text>Next Js for beginner</Text>
-                    </Stack>
-                </Card>
-                <Card p='2' _hover={{ transform: 'translateY(-2px)', transition: 'all .2s ease-in-out', shadow: 'md', cursor: 'pointer' }}>
-                    <Stack direction='row' align='center' spacing='4'>
-                        <FaInstagram />
-                        <Text size={{ base: 'sm', md: 'unset' }}>Next Js for beginner</Text>
-                    </Stack>
-                </Card>
-                <Card p='2' _hover={{ transform: 'translateY(-2px)', transition: 'all .2s ease-in-out', shadow: 'md', cursor: 'pointer' }}>
-                    <Stack direction='row' align='center' spacing='4'>
-                        <FaLinkedin />
-                        <Text size={{ base: 'sm', md: 'unset' }}>Next Js for beginner</Text>
-                    </Stack>
-                </Card>
-                <Card p='2' _hover={{ transform: 'translateY(-2px)', transition: 'all .2s ease-in-out', shadow: 'md', cursor: 'pointer' }}>
-                    <Stack direction='row' align='center' spacing='4'>
-                        <FaFacebook />
-                        <Text size={{ base: 'sm', md: 'unset' }}>Next Js for beginner</Text>
-                    </Stack>
-                </Card>
-                <Card p='2' _hover={{ transform: 'translateY(-2px)', transition: 'all .2s ease-in-out', shadow: 'md', cursor: 'pointer' }}>
-                    <Stack direction='row' align='center' spacing='4'>
-                        <FaInstagram />
-                        <Text size={{ base: 'sm', md: 'unset' }}>Next Js for beginner</Text>
-                    </Stack>
-                </Card>
+                {
+                    isPending ?
+                        <Card p='2' _hover={{ transform: 'translateY(-2px)', transition: 'all .2s ease-in-out', shadow: 'md', cursor: 'pointer' }}>
+                            <Text>waiting for data ...</Text>
+                        </Card>
+                        :
+                        isError ?
+                            <Card p='2' bgColor='red.400' _hover={{ transform: 'translateY(-2px)', transition: 'all .2s ease-in-out', shadow: 'md', cursor: 'pointer' }}>
+                                <Text>{error?.message}</Text>
+                            </Card>
+                            :
+                            (data && data.length > 0) ? data.map((lastContent) => (
+                                <Card key={lastContent.topic.id} p='2' _hover={{ transform: 'translateY(-2px)', transition: 'all .2s ease-in-out', shadow: 'md', cursor: 'pointer' }}>
+                                    <Stack direction='row' align='center' spacing='4'>
+                                        {getPlatform(lastContent.plateformname)}
+                                        <Text isTruncated>{lastContent.topic.topic}</Text>
+                                    </Stack>
+                                </Card>
+                            ))
+                                :
+                                <CardBody>
+                                    <NoContentText />
+                                </CardBody>
+                }
             </Stack>
         </Card>
     )
